1b2c2e3e3085b0e0ac093d975678dbd4053f5fb9
Amin Kasirifar <aminkdev@gmail.com>
2020-04-12 11:25:40 +0430

    added in and contains operators

--- src/PayamGostarApiClient/CrmObject/Constants.cs
+++ src/PayamGostarApiClient/CrmObject/Constants.cs
@@ -17,17 +17,17 @@ namespace Septa.PayamGostarApiClient.CrmObject
         Equals = 0,
         GreateThan = 1,
         GreaterThanOrEqual = 2,
-        //In = 3,
+        In = 3,
         LessThan = 4,
         LessThanOrEqual = 5,
         NotEqual = 6,
-        //NotIn = 7,
+        NotIn = 7,
         //Expression = 8, // comparing two specific column values => "" expression {"columnA" equals "columnB"}
         //Modulo = 9, // value of a field divided by a divisor has the specified remainder => Field modulo {"divisor","remainder"}
         //Regex = 10,
-        //TextStartsWith = 11,
-        //TextContains = 12,
-        //TextEndsWith = 13,
+        TextStartsWith = 11,
+        TextContains = 12,
+        TextEndsWith = 13,
         //All = 14, // value of a field is an array that contains all the specified elements
         //Length = 15, // matches any array with the number of elements specified by the argument
     }
